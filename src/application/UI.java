ec87b404a2b5b32c433022a199ce1340d6fda533
Mauricio Trampo <mauriciozaccaro@gmail.com>
2021-02-15 17:00:00 -0400

    Feito no trabalho: Chess layer and printing the board - Foi feito a impressão do tabuleiro vazio

---
+++ src/application/UI.java
@@ -0,0 +1,29 @@
+package application;
+
+import chess.ChessPiece;
+
+public class UI {
+	
+	public static void printBoard(ChessPiece[][] pieces) {
+		
+		for(int i = 0; i < pieces.length; i++ ) {
+			System.out.print((8 - i) + " "); // caso precise lembrar.. aqui começa com (8 - i) que é (8 - 0) = 8
+			for(int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("  A B C D F E G H");
+		
+	}
+	
+	private static void printPiece(ChessPiece piece) {
+		if(piece == null) {
+			System.out.print("-");
+		}
+		else {
+			System.out.print(piece);
+		}
+		System.out.print(" ");			
+	}
+}
